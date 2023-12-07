8b272b4eae73eb44631af0594bdccc4c430e2477
Rubén <rubencelislobato@gmauil.com>
2023-12-07 17:30:52 +0100

    Realización advent_of_code_1 parte 1

---
+++ src/main/kotlin/advent_of_code_1.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    val puzles = readInput("Day01")
+    val sumaDeValores = calcularCalibracionDeValores(puzles)
+    println("La suma de todos los valores es: $sumaDeValores")
+
+}
+
+fun calcularCalibracionDeValores(listaDeCadenas: List<String>): Int {
+    val listaDigitos = mutableListOf<Int>()
+
+    for (linea in listaDeCadenas) {
+        var numerosLinea = ""
+        for (caracter in linea) {
+            if (caracter.isDigit()) {
+                numerosLinea += caracter
+            }
+        }
+        val primerDigito = numerosLinea.first()
+        val segundoDigito = numerosLinea.last()
+        val digitoCreado = "$primerDigito$segundoDigito".toInt()
+        listaDigitos.add(digitoCreado)
+
+    }
+
+    return listaDigitos.sum()
+}
