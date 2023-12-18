1e08bdce6469b5b8166110d239de442f72aad803
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-12-18 16:59:37 +0100

    Cambios realizados en advent_of_code_11.kt parte 1 y parte 2

--- src/main/kotlin/advent_of_code_11.kt
+++ src/main/kotlin/advent_of_code_11.kt
@@ -118,54 +118,112 @@
 // Expande el universo y luego halla la longitud del camino más corto entre cada par de
 // galaxias. ¿Cuál es la suma de estas longitudes?
 
+import kotlin.math.abs
+import kotlin.math.min
+import kotlin.math.max
+
 fun main() {
-    val espacioVacio = "."
-    val galaxia = "#"
-    val espacio = readInput("Day11")
+    val input = readInput("Day11")
 
-    val numerosDeGalaxias = buscarGalaxias(espacio, espacioVacio, galaxia)
-    val sumaDeLongitudes = calcularSumaDeLongitudes(espacio, numerosDeGalaxias)
-    println(sumaDeLongitudes)
-}
+    val filasVacias = encontrarFilasVacias(input)
+    val columnasVacias = encontrarColumnasVacias(input)
+    val galaxias = encontrarGalaxias(input)
 
-fun buscarGalaxias(espacio: List<String>, espacioVacio: String, galaxia: String): Map<Pair<Int, Int>, Int> {
-    val numerosDeGalaxias = mutableMapOf<Pair<Int, Int>, Int>()
-    var numeroGalaxia = 1
+    val tamanoExpansion1 = 1L
+    val tamanoExpansion2 = 1000000L - 1
+    var sumaCaminoMasCorto1 = 0L
+    var sumaCaminoMasCorto2 = 0L
 
-    for (i in espacio.indices) {
-        for (j in espacio[i].indices) {
-            if (espacio[i][j] == galaxia[0]) {
-                numerosDeGalaxias[Pair(i, j)] = numeroGalaxia
-                numeroGalaxia++
-            }
+    // Bucle para comparar todas las combinaciones de pares de galaxias
+    for (i in galaxias.indices) {
+        for (j in i + 1 until galaxias.size) {
+            val columnasExpandidas = contarColumnasExpandidas(galaxias[i], galaxias[j], columnasVacias)
+            val filasExpandidas = contarFilasExpandidas(galaxias[i], galaxias[j], filasVacias)
+            // Calcular distancia entre galaxias
+            val camino = calcularCamino(galaxias[i], galaxias[j])
+            // Sumar distancias considerando expansiones y agregar al resultado final
+            sumaCaminoMasCorto1 += camino + filasExpandidas * tamanoExpansion1 + columnasExpandidas * tamanoExpansion1
+            sumaCaminoMasCorto2 += camino + filasExpandidas * tamanoExpansion2 + columnasExpandidas * tamanoExpansion2
         }
     }
 
-    return numerosDeGalaxias
+    val solucion1 = sumaCaminoMasCorto1.toString()
+    val solucion2 = sumaCaminoMasCorto2.toString()
+
+    println("La suma de las longitudes es $solucion1")
+    println("La suma de estas longitudes es $solucion2")
 }
 
-fun calcularSumaDeLongitudes(espacio: List<String>, numerosDeGalaxias: Map<Pair<Int, Int>, Int>): Int {
-    val filas = espacio.size
-    val columnas = espacio[0].length
-    var sumaDeLongitudes = 0
-
-    for ((posicion, numero) in numerosDeGalaxias) {
-        for (i in 0 until filas) {
-            for (j in 0 until columnas) {
-                val otraGalaxia = Pair(i, j)
-                val otraNumero = numerosDeGalaxias[otraGalaxia] ?: continue
-
-                if (numero != otraNumero) {
-                    val distancia = calcularDistancia(posicion, otraGalaxia)
-                    sumaDeLongitudes += distancia
-                }
-            }
+// Encontrar filas vacías en la entrada
+fun encontrarFilasVacias(input: List<String>): List<Int> {
+    val filasVacias = mutableListOf<Int>()
+    for (y in input.indices) {
+        if (input[y].all { it == '.' }) {
+            filasVacias.add(y)
         }
     }
-
-    return sumaDeLongitudes
+    return filasVacias
 }
 
-fun calcularDistancia(galaxia1: Pair<Int, Int>, galaxia2: Pair<Int, Int>): Int {
-    return kotlin.math.abs(galaxia1.first - galaxia2.first) + kotlin.math.abs(galaxia1.second - galaxia2.second)
+// Encontrar columnas vacías en la entrada
+fun encontrarColumnasVacias(input: List<String>): List<Int> {
+    val columnasVacias = mutableListOf<Int>()
+    for (x in input[0].indices) {
+        if (input.map { linea -> linea[x] }.all { it == '.' }) {
+            columnasVacias.add(x)
+        }
+    }
+    return columnasVacias
 }
+
+// Encontrar coordenadas de las galaxias en la entrada
+fun encontrarGalaxias(input: List<String>): List<Pair<Int, Int>> {
+    val galaxias = mutableListOf<Pair<Int, Int>>()
+    for (y in input.indices) {
+        for (x in input[0].indices) {
+            if (input[y][x] == '#') galaxias.add(Pair(y, x))
+        }
+    }
+    return galaxias
+}
+
+// Contar columnas expandidas entre dos galaxias
+fun contarColumnasExpandidas(galaxia1: Pair<Int, Int>, galaxia2: Pair<Int, Int>, columnasVacias: List<Int>): Long {
+    return columnasVacias.count {
+        it > min(galaxia1.second, galaxia2.second)
+                && it < max(galaxia1.second, galaxia2.second)
+    }.toLong()
+}
+
+// Contar filas expandidas entre dos galaxias
+fun contarFilasExpandidas(galaxia1: Pair<Int, Int>, galaxia2: Pair<Int, Int>, filasVacias: List<Int>): Long {
+    return filasVacias.count {
+        it > min(galaxia1.first, galaxia2.first)
+                && it < max(galaxia1.first, galaxia2.first)
+    }.toLong()
+}
+
+// Calcular la distancia entre dos galaxias
+fun calcularCamino(galaxia1: Pair<Int, Int>, galaxia2: Pair<Int, Int>): Long {
+    return (abs(galaxia1.second - galaxia2.second) + abs(galaxia1.first - galaxia2.first)).toLong()
+}
+
+// ---- Parte 2 -----
+
+// Las galaxias son mucho más antiguas (y, por lo tanto, mucho más distantes) de lo que
+// el investigador estimó inicialmente.
+
+// Ahora, en lugar de la expansión que realizaste anteriormente, haz que cada fila o
+// columna vacía sea un millón de veces más grande. Es decir, cada fila vacía debe
+// reemplazarse con 1000000 filas vacías, y cada columna vacía debe reemplazarse con
+// 1000000 columnas vacías.
+
+// (En el ejemplo anterior, si cada fila o columna vacía fuera simplemente 10 veces
+// más grande, la suma de las rutas más cortas entre cada par de galaxias sería 1030.
+// Si cada fila o columna vacía fuera simplemente 100 veces más grande, la suma de las
+// rutas más cortas entre cada par de galaxias sería 8410. Sin embargo, tu universo
+// deberá expandirse mucho más allá de estos valores).
+
+// Comenzando con la misma imagen inicial, expande el universo según estas nuevas
+// reglas, luego encuentra la longitud de la ruta más corta entre cada par de galaxias.
+// ¿Cuál es la suma de estas longitudes?
