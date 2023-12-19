677d593ac5c8407ea97b7454bb7a615ed1e597fd
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-12-19 17:55:02 +0100

    Realizacion de la parte 1 de advent_of_code_14.kt

---
+++ src/main/kotlin/advent_of_code_14.kt
@@ -0,0 +1,105 @@
+// --- Día 14: Plato Reflector Parabólico ---
+
+// --- Parte 1 -----
+
+// Llegas al lugar donde todos los espejos estaban apuntando: un masivo plato reflector parabólico
+// adherido al costado de otra gran montaña.
+
+// El plato está compuesto por muchos espejos pequeños, pero mientras que los espejos en sí mismos
+// tienen aproximadamente la forma de un plato reflector parabólico, cada espejo individual parece
+// apuntar ligeramente en la dirección equivocada. Si el plato está destinado a concentrar la luz,
+// en este momento solo la está enviando en una dirección vaga.
+
+// ¡Este sistema debe ser el que proporciona la energía para la lava! Si enfocas el plato reflector,
+// tal vez puedas ir a donde está apuntando y usar la luz para corregir la producción de lava.
+
+// Al examinar más de cerca, parece que cada espejo individual está conectado mediante un elaborado
+// sistema de cuerdas y poleas a una gran plataforma metálica debajo del plato. La plataforma está
+// cubierta de grandes rocas de varias formas. Dependiendo de su posición, el peso de las rocas deforma
+// la plataforma, y la forma de la plataforma controla qué cuerdas se mueven y, en última instancia,
+// el enfoque del plato.
+
+// En resumen: si mueves las rocas, puedes enfocar el plato. ¡La plataforma incluso tiene un panel de
+// control en el lateral que te permite inclinarla en una de las cuatro direcciones! Las rocas redondas
+// (O) rodarán cuando se incline la plataforma, mientras que las rocas en forma de cubo (#) permanecerán
+// en su lugar. Toma nota de las posiciones de todos los espacios vacíos (.) y las rocas (tu entrada de
+// rompecabezas). Por ejemplo:
+
+// O....#....
+// O.OO#....#
+// .....##...
+// OO.#O....O
+// .O.....O#.
+// O.#..O.#.#
+// ..O..#O..O
+// .......O..
+// #....###..
+// #OO..#....
+
+// Comienza inclinando la palanca para que todas las rocas se deslicen hacia el norte tanto como sea
+// posible:
+
+// OOOO.#.O..
+// OO..#....#
+// OO..O##..O
+// O..#.OO...
+// ........#.
+// ..#....#.#
+// ..O..#.O.O
+// ..O.......
+// #....###..
+// #....#....
+
+// Notas que las vigas de soporte a lo largo del lado norte de la plataforma están dañadas; para
+// asegurarte de que la plataforma no se colapse, debes calcular la carga total en las vigas de
+// soporte del norte.
+
+// La cantidad de carga causada por una sola roca redonda (O) es igual al número de filas desde la roca
+// hasta el borde sur de la plataforma, incluida la fila en la que se encuentra la roca. (Las rocas en
+// forma de cubo (#) no contribuyen a la carga). Entonces, la cantidad de carga causada por cada roca
+// en cada fila es la siguiente:
+
+// OOOO.#.O.. 10
+// OO..#....# 9
+// OO..O##..O 8
+// O..#.OO... 7
+// ........#. 6
+// ..#....#.# 5
+// ..O..#.O.O 4
+// .O....... 3
+// #....###.. 2
+// #....#.... 1
+
+// La carga total es la suma de la carga causada por todas las rocas redondas. En este ejemplo, la
+// carga total es 136.
+
+// Inclina la plataforma para que todas las rocas redondas rueden hacia el norte. Después, ¿cuál es la
+// carga total en las vigas de soporte del norte?
+
+import java.io.File
+
+fun main() {
+    val plataforma = leerEntrada().lines().map { it.toCharArray().toTypedArray() }.toTypedArray()
+    var peso = 0
+
+    for (y in plataforma.indices) {
+        for (x in plataforma[y].indices) {
+            if (plataforma[y][x] == 'O') {
+                plataforma[y][x] = '.'
+                var i = y - 1
+                while (i >= 0 && plataforma[i][x] == '.') {
+                    i--
+                }
+                plataforma[i + 1][x] = 'O'
+                peso += plataforma.size - i - 1
+            }
+        }
+    }
+
+    println("Carga total: $peso")
+}
+
+fun leerEntrada(): String {
+    // Ajusta la ruta del archivo según sea necesario
+    return File("src/Day14.txt").readText().trim()
+}
